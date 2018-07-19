c94c8767d50293237babbf9b49c4d1f037ca7b0e
Alon Gamliel <alon.gamliel@fluxfederation.com>
2018-07-20 09:18:58 +1200

    Add WithDraggableModel HOC

--- src/app/components/diagram/Node.tsx
+++ src/app/components/diagram/Node.tsx
@@ -2,14 +2,18 @@ import * as React from "react";
 import {observer} from "mobx-react";
 import styled from "styled-components";
 import NodeModel from "./NodeModel";
+import withDraggableModel from "../../../shared_modules/Draggable";
 
 const StyledNode = styled.circle`
+  cursor: move
 `;
+
+const DraggableStyledNode = withDraggableModel(StyledNode);
 
 function Node({node}: { node: NodeModel }) {
   return (
-    <StyledNode r="1" cx={node.x} cy={node.y}>
-    </StyledNode>
+    <DraggableStyledNode r="1" cx={node.x} cy={node.y} model={node}>
+    </DraggableStyledNode>
   );
 }
 
