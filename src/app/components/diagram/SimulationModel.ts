6358b3b6910d29779dc3e7bf09600127ca0b8249
Alon Gamliel <alon.gamliel@fluxfederation.com>
2018-09-20 11:23:34 +1200

    Handle manually d3 simulation ticks

--- src/app/components/diagram/SimulationModel.ts
+++ src/app/components/diagram/SimulationModel.ts
@@ -1,34 +1,64 @@
 import * as d3 from "d3-force";
 import NodeModel from "./NodeModel";
 import LinkModel from "./LinkModel";
-import {action, observable} from "mobx";
+import {action, computed, observable} from "mobx";
 
 const CHARGE_FORCE_STRENGTH = -0.5;
 const COLLIDE_FORCE_STRENGTH = 0.5;
 const LINK_DISTANCE = 10;
+const ALPHA_INITIAL = 1;
 const ALPHA_MIN = 0.001;
 const ALPHA_TARGET_WHEN_NOT_DRAGGING = 0;   // must be < ALPHA_MIN
 const ALPHA_TARGET_WHEN_DRAGGING = 0.3;     // must be > ALPHA_MIN
 
 class SimulationModel {
   private simulation;
-  @observable running = true;
+  @observable animationFrameId = 0;
 
   init(nodes: NodeModel[], links: LinkModel[]) {
     this.simulation = d3.forceSimulation();
     this.simulation
       .nodes(nodes)
+      .force("centering_force", d3.forceCenter())
       .force("charge_force", d3.forceManyBody().strength(CHARGE_FORCE_STRENGTH))
       .force("links_force", d3.forceLink(links).distance(LINK_DISTANCE))
       .force("collide_force", d3.forceCollide().strength(COLLIDE_FORCE_STRENGTH).radius(1.5))
+      .alpha(ALPHA_INITIAL)
       .alphaMin(ALPHA_MIN)
-      .on('end', () => { this.running = false } );
+      .alphaTarget(ALPHA_TARGET_WHEN_NOT_DRAGGING)
+      .stop();
+
+    this.restartSimulation();
+  }
+
+  @action.bound
+  restartSimulation() {
+    this.stopSimulation();
+    if (this.simulation.alpha() >= this.simulation.alphaMin())
+      this.animationFrameId = window.requestAnimationFrame(this.tickSimulation);
+  }
+
+  @action.bound
+  tickSimulation() {
+    this.simulation.tick();
+    this.restartSimulation();
+  }
+
+  @action.bound
+  stopSimulation() {
+    if (this.isRunning)
+      window.cancelAnimationFrame(this.animationFrameId);
+    this.animationFrameId = 0;
+  }
+
+  @computed get isRunning() {
+    return this.animationFrameId != 0;
   }
 
   @action.bound
   notifyDragStarted() {
-    this.simulation.alphaTarget(ALPHA_TARGET_WHEN_DRAGGING).restart();
-    this.running = true;
+    this.simulation.alphaTarget(ALPHA_TARGET_WHEN_DRAGGING).alpha(ALPHA_INITIAL);
+    this.restartSimulation();
   }
 
   @action.bound
