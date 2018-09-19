4deb7411cc33a4774284fd07a815fac268e4c013
Alon Gamliel <alon.gamliel@fluxfederation.com>
2018-09-20 11:38:12 +1200

    Upgraded MobX version; enforced strict mode; updated package.json info.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import 'bulma/css/bulma.css';
-
 import MainPage from "./pages/MainPage/index";
+import {configure} from "mobx";
+
+configure({enforceActions: "observed"});
 
 ReactDOM.render(<MainPage/>, document.getElementById('app-root'));
