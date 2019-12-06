f2c652a41e5388d69b7eb3f7e3f8d9b49bd75f51
Alon Gamliel <alon.gamliel@fluxfederation.com>
2019-12-06 17:13:13 +1300

    WIP

--- src/shared_modules/WithDraggableViewBox.tsx
+++ src/shared_modules/WithDraggableViewBox.tsx
@@ -2,12 +2,34 @@ import * as React from "react";
 import {FunctionComponent, SVGProps, useCallback} from "react";
 import WithDraggableSVG from "./WithDraggableSVG";
 
-type WithDraggableViewBox<P> = FunctionComponent<P>
+type WithViewBox<P> = P & { viewBox?: string }
 
-function WithDraggableViewBox<P extends SVGProps<SVGElement>>(Component: React.ComponentType<P> | string): WithDraggableViewBox<P> {
+type WithDraggableViewBox<P> = FunctionComponent<WithViewBox<P>>
+
+function WithDraggableViewBox<P extends SVGProps<SVGElement>>(Component: React.ComponentType<WithViewBox<P>> | string): WithDraggableViewBox<P> {
   const DraggableContainer = WithDraggableSVG(Component);
 
-  const Wrapper: WithDraggableViewBox<P> = (props: P) => {
+  // continue... remove the last commit from the log. the regex doesn't work for negative numbers. also not for decimal numbers (with dot). is this the right way...?
+  // also check that "none" viewbox doesn't get changed by updateViewBox.
+  const viewBoxRegex = /^\s*(?:(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)|none)\s*$/.compile()
+
+  function updateViewBox(viewBox?: string): string | undefined {
+    if (typeof (viewBox) !== "string") {
+      return viewBox
+    }
+    const match = viewBox.match(viewBoxRegex)
+    if (!match || (match[0] === "none")) {
+      return viewBox
+    }
+    debugger;
+    const minX = parseFloat(match[0]);
+    const minY = parseFloat(match[1]);
+    const width = parseFloat(match[2]);
+    const height = parseFloat(match[3]);
+    return `${minX} ${minY} ${width} ${height}`;
+  }
+
+  const Wrapper: WithDraggableViewBox<P> = ({viewBox, ...restProps}: P) => {
     const onSVGDragStarted = useCallback((x: number, y: number) => {
       console.log(`drag started (${x}, ${y})!`);
     }, [])
@@ -21,7 +43,8 @@ function WithDraggableViewBox<P extends SVGProps<SVGElement>>(Component: React.C
     }, [])
 
     return <DraggableContainer
-      {...props}
+      {...restProps as P}
+      viewBox={updateViewBox(viewBox)}
       onSVGDragStarted={onSVGDragStarted}
       onSVGDrag={onSVGDrag}
       onSVGDragEnded={onSVGDragEnded}
